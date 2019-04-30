8101f6c23a8c4ce843995f2f4062998b459eb675
LastZZZ <lastmegumi@gmail.com>
2019-04-30 17:39:40 -0400

    singleton class

--- mongodb.php
+++ mongodb.php
@@ -1,19 +1,43 @@
 <?php
 class _MongoDB{
-	function __construct(){
-		$this->mcon = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+	private static $instance = null;
+
+	private function __construct(){
+		$password = "pwd";
+		$connect_string = "mongodb+srv://lastzzz:{$password}@cluster0-wnwag.gcp.mongodb.net/test?retryWrites=true";
+		//$connect_string = "mongodb://localhost:27017";
+		$this->mcon = new MongoDB\Driver\Manager($connect_string);
 		$this->database = "test";
 		$this->table = "sites";
 		$this->d_t = $this->database . '.' . $this->table;
 	}
 
+	public function __clone(){
+        //throw error
+        trigger_error("Can't clone object",E_USER_ERROR);
+    }    
+
+	//public static function getInstance()
+	public static function init()
+	{
+		if (self::$instance == null)
+		{
+		  self::$instance = new _MongoDB();
+		}
+		return self::$instance;
+	}
+
+	static function _id($id = null){
+		return new MongoDB\BSON\ObjectId($id);
+	}
+
 	function setTable($table){		
-		$this->table = $table;
+		$this->table = $table; 
 		$this->d_t = $this->database . '.' . $this->table;
 	}
 
 	function setDatabase($database){
-		$this->database = "test"; 
+		$this->database = $database; 
 		$this->d_t = $this->database . '.' . $this->table;
 	}
 
@@ -22,30 +46,35 @@ class _MongoDB{
 	function currentDatabase(){return $this->database;}
 
 	function save($document = []){
+		if(!$document){
+			foreach (get_object_vars($this) as $key => $value) {
+				$document[$key]	= $value;
+			}
+		}
 		$bulk = new MongoDB\Driver\BulkWrite;
 		$_id= $bulk->insert($document);
-		var_dump($_id);
+		//var_dump($_id);
 		$writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
 		$result = $this->mcon->executeBulkWrite($this->d_t, $bulk, $writeConcern);
+		return $_id;
 	}
 
 	function saveAll($documents = array()){
-		if(!$documents){ return;	}
+		if(!$documents){ return;}
 		$bulk = new MongoDB\Driver\BulkWrite;
 		foreach($documents as $doc):
 			$bulk->insert($doc);
 		endforeach;
-		$this->mcon->executeBulkWrite($this->d_t, $bulk);
+		return $this->mcon->executeBulkWrite($this->d_t, $bulk);
 	}
 
 	function findAll($filter = [], $options = []){
-
 		$query = new MongoDB\Driver\Query($filter, $options);
 		$cursor = $this->mcon->executeQuery($this->d_t, $query);
 		foreach ($cursor as $document) {
 		    $arr[] = $document;
 		}
-		return $arr;
+		return @$arr? $arr : null;
 	}
 
 	function find($filter = [], $options = [], $join = []){
@@ -71,6 +100,7 @@ class _MongoDB{
 		);
 		$writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
 		$result = $this->mcon->executeBulkWrite($this->d_t, $bulk, $writeConcern);
+		return $result;
 	}
 
 	function delete($filter = [], $options = ['limit' => 1]){
@@ -78,21 +108,11 @@ class _MongoDB{
 		$bulk->delete($filter, $options);
 		$writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
 		$result = $this->mcon->executeBulkWrite($this->d_t, $bulk, $writeConcern);
+		return $result;
 	}
 
 	function deleteAll($filter = []){
-		$this->delete($filter, ['limit' => 0]);
+		return $this->delete($filter, ['limit' => 0]);
 	}
-}
-
-$md = new _MongoDB();
-try {
-	
-$res = $md->find([],[],["table" =>'runoob']);
-print_r($res);
-//$md->setTable("runoob");
-//$md->save(['sites_id'	=>	$res->_id, "name"	=>	"ceshi"]);
-} catch (Exception $e) {
-	print_r($e->getMessage());
 }
 ?>
